3963f3ca75f8079a23773907bcf4e5677031d5f9
Chris Deuda <chris@sevron.co.uk>
2026-06-10 06:02:59 +0800

    Revert "chore: update vilina architecture submodule"
    This reverts commit 6824d535fbadfd64bb7e5d18d38bde3d81bcae5a.

--- 2026-03-05-ralph-spec/tests/setup.js
+++
@@ -1,7 +0,0 @@
-import { beforeEach } from 'vitest';
-import { Window } from 'happy-dom';
-
-// Initialize happy-dom window for localStorage
-const window = new Window();
-global.localStorage = window.localStorage;
-global.window = window;
